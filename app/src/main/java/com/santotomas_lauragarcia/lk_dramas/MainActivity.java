991056a9c1af02cd0084c3c8690358293a6784d2
serviTch <135564958+serviTch@users.noreply.github.com>
2023-10-04 14:19:29 -0300

    Initial commit

---
+++ app/src/main/java/com/santotomas_lauragarcia/lk_dramas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.santotomas_lauragarcia.lk_dramas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
